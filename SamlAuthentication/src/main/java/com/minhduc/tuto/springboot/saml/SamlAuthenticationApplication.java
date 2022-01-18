ef79b8fe9f78ff208a2a3ddf679084e212935857
Minh Duc Ngo <minh.duc.ngo@volkswagen-infotainment.com>
2022-01-18 12:09:16 +0100

    Springboot saml2 autehntication

---
+++ SamlAuthentication/src/main/java/com/minhduc/tuto/springboot/saml/SamlAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.minhduc.tuto.springboot.saml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlAuthenticationApplication {
+
+    public static void main(String[] args) {
+	SpringApplication.run(SamlAuthenticationApplication.class, args);
+    }
+
+}
---
+++ SamlAuthentication/src/test/java/com/minhduc/tuto/springboot/saml/SamlAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhduc.tuto.springboot.saml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamlAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
