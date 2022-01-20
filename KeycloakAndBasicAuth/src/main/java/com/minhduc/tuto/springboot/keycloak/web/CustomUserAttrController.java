c199cdecb4dcf246b42d736a780b45fc8d343d56
Minh Duc Ngo <wunsch85@gmail.com>
2022-01-20 14:39:24 +0100

    remove warning

--- KeycloakAndBasicAuth/src/main/java/com/minhduc/tuto/springboot/keycloak/web/CustomUserAttrController.java
+++ KeycloakAndBasicAuth/src/main/java/com/minhduc/tuto/springboot/keycloak/web/CustomUserAttrController.java
@@ -16,8 +16,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.minhduc.tuto.springboot.keycloak.config.CustomKeycloakAuthenticationProvider;
-
 @Controller
 public class CustomUserAttrController {
     
