6721bfbd81304b124619306cd777125c0f967246
Minh Duc Ngo <wunsch85@gmail.com>
2022-01-21 08:04:52 +0100

    handle nullpointerexception

--- KeycloakAndBasicAuth/src/main/java/com/minhduc/tuto/springboot/keycloak/web/CustomUserAttrController.java
+++ KeycloakAndBasicAuth/src/main/java/com/minhduc/tuto/springboot/keycloak/web/CustomUserAttrController.java
@@ -18,46 +18,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomUserAttrController {
-    
+
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomUserAttrController.class.getName());
 
-    @SuppressWarnings("unchecked")
     @GetMapping(path = "/users")
     public String getUserInfo(Model model) {
 	LOGGER.info("get user info");
-	KeycloakAuthenticationToken authentication = (KeycloakAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-	final Principal principal = (Principal) authentication.getPrincipal();
-
-	if (principal != null) {
-	    // user has a valid session, we can assign role on the fly like this
-
-	}
-	String dob = "";
-	if (principal instanceof KeycloakPrincipal) {
-	    KeycloakPrincipal<KeycloakSecurityContext> kPrincipal = (KeycloakPrincipal<KeycloakSecurityContext>) principal;
-	    IDToken token = kPrincipal.getKeycloakSecurityContext().getIdToken();
-	    Map<String, Object> customClaims = token.getOtherClaims();
-
-	    if (customClaims.containsKey("DOB")) {
-		dob = String.valueOf(customClaims.get("DOB"));
-	    }
-	    for (String key : customClaims.keySet()) {
-		System.out.println(key + " = " + customClaims.get(key));
-	    }
-	    model.addAttribute("KeycloakRoles", kPrincipal.getKeycloakSecurityContext().getToken().getRealmAccess().getRoles());
-	}
-
-	Set<String> roles = authentication.getAuthorities().stream().map(r -> r.getAuthority()).collect(Collectors.toSet());
-
-	model.addAttribute("username", principal.getName());
-	model.addAttribute("dob", dob);
-	model.addAttribute("AppRoles", roles);
+	getUserDetails(model);
 	return "userInfo";
     }
 
-    @SuppressWarnings("unchecked")
     @GetMapping(path = "/admin")
     public String getAdminInfo(Model model) {
+	model = getUserDetails(model);
+	return "admin";
+    }
+
+    private Model getUserDetails(Model model) {
 	KeycloakAuthenticationToken authentication = (KeycloakAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
 	final Principal principal = (Principal) authentication.getPrincipal();
 
@@ -67,22 +44,30 @@ public class CustomUserAttrController {
 	}
 	String dob = "";
 	if (principal instanceof KeycloakPrincipal) {
+	    @SuppressWarnings("unchecked")
 	    KeycloakPrincipal<KeycloakSecurityContext> kPrincipal = (KeycloakPrincipal<KeycloakSecurityContext>) principal;
 	    IDToken token = kPrincipal.getKeycloakSecurityContext().getIdToken();
-	    Map<String, Object> customClaims = token.getOtherClaims();
+	    if (token != null) {
+		Map<String, Object> customClaims = token.getOtherClaims();
 
-	    if (customClaims.containsKey("DOB")) {
-		dob = String.valueOf(customClaims.get("DOB"));
+		if (customClaims.containsKey("DOB")) {
+		    dob = String.valueOf(customClaims.get("DOB"));
+		}
+		for (String key : customClaims.keySet()) {
+		    LOGGER.info(key + " = " + customClaims.get(key));
+		}
+		if (kPrincipal.getKeycloakSecurityContext().getToken() != null
+		        && kPrincipal.getKeycloakSecurityContext().getToken().getRealmAccess() != null) {
+		    model.addAttribute("KeycloakRoles", kPrincipal.getKeycloakSecurityContext().getToken().getRealmAccess().getRoles());
+		}
 	    }
-	    model.addAttribute("KeycloakRoles", kPrincipal.getKeycloakSecurityContext().getToken().getRealmAccess().getRoles());
 	}
 
 	Set<String> roles = authentication.getAuthorities().stream().map(r -> r.getAuthority()).collect(Collectors.toSet());
 
 	model.addAttribute("username", principal.getName());
 	model.addAttribute("dob", dob);
-	model.addAttribute("AppRoles", roles);
-	return "admin";
+	return model.addAttribute("AppRoles", roles);
     }
 
 }
